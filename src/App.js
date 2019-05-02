338d334b97e31700f5e874bd642d0930a3ece89b
Douglas Scriptore <douglass@rpc.com.br>
2019-05-02 16:25:19 -0300

    erro eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-    Hello World
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
